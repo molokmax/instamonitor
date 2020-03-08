6eca0fc160dbf9413900db35115ad88b6b82f75f
Maxim Molokanov <maks.molokanov@gmail.com>
2020-03-08 13:19:44 +0300

    added maintains scripts

---
+++ tools/add_cron_job.sh
@@ -0,0 +1,10 @@
+echo root >> /etc/cron.allow
+# echo molokmax >> /etc/cron.allow
+
+touch /var/spool/cron/crontabs/root
+/usr/bin/crontab /var/spool/cron/crontabs/root
+
+# touch /var/spool/cron/molokmax
+# /usr/bin/crontab /var/spool/cron/molokmax
+
+echo "0 0 * * * /opt/instamonitor/InstaMonitor" >> /var/spool/cron/crontabs/root
---
+++ tools/build_orangepi.ps1
@@ -0,0 +1,4 @@
+cd ../src
+dotnet publish -c release --self-contained -r linux-arm
+# linux-arm64 linux-arm linux-x64
+cd ../tools
